bc1b2aefa3ab399265f4261cd2522c3ff8cd30d0
Ubuntu <[imrtky@gmail.com]>
2020-06-15 08:03:04 +0000

    Userモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   has_many :videos, dependent: :destroy
   before_save   :downcase_email
   validates :name, presence: true, length: { maximum: 50 }
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6}, allow_nil: true
+  validates :password, presence: true, length: { in: 6..128}, allow_nil: true
 end
 
 def downcase_email
