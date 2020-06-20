55335c9166a3ab8da08eadac1b328ff873f083a6
Ubuntu <[imrtky@gmail.com]>
2020-06-20 05:52:26 +0000

    #36 close

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,28 +2,20 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:show]
 
-  # GET /users
-  # GET /users.json
   def index
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     @videos = Video.all.where(user_id: current_user.id).page(params[:page]).per(20)
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -39,11 +31,10 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       if User.exists?(id: params[:id])
-        correct_user
         @user = User.find(params[:id])
+        correct_user
       else
         redirect_to root_path
       end
@@ -53,7 +44,6 @@ class UsersController < ApplicationController
       redirect_to root_path unless @user && current_user == @user
     end
 
-    # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:name)
     end
