e5f924ca9aad7e0d994c13acd801ddc9a27ef241
Ubuntu <[imrtky@gmail.com]>
2020-06-20 04:48:00 +0000

    users#showへのアクセス関連修正 #35

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:show]
 
   # GET /users
   # GET /users.json
@@ -9,7 +10,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    correct_user
     @videos = Video.all.where(user_id: current_user.id).page(params[:page]).per(20)
   end
 
@@ -38,34 +38,11 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       if User.exists?(id: params[:id])
+        correct_user
         @user = User.find(params[:id])
       else
         redirect_to root_path
@@ -73,7 +50,7 @@ class UsersController < ApplicationController
     end
     
     def correct_user
-      redirect_to root_path unless current_user == @user
+      redirect_to root_path unless @user && current_user == @user
     end
 
     # Only allow a list of trusted parameters through.
